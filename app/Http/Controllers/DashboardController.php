15ed645f2a160c0132db5c4c7615c30cde7aa893
shorayaau <xrosearebluex@gmail.com>
2023-04-30 23:51:01 +0700

    test ya

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    function index()
+    {
+        return view('dashboard')->with([
+            'user' => Auth::user()
+        ]);;
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+
+@section('content')
+CRUD - POS
+@endsection
